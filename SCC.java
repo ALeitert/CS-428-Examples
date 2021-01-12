c0cc325b930b0812b4892d2081625204c3daab64
Arne (Wahrstorf) <aleitert@kent.edu>
2021-01-12 14:40:55 +0100

    Added class with DFS implementation (will become function for strongly connected components).

---
+++ SCC.java
@@ -0,0 +1,96 @@
+
+public class SCC
+{
+    public static int[][] strConComps(Graph g, int startId)
+    {
+        // Output data
+        // 0: parent-IDs of DFS-tree
+        // 1: pre-order
+        // 2: post-order
+
+        int noOfVertices = g.noOfVertices;
+
+        int[] parIds = new int[noOfVertices];
+
+        int[] preOrder = new int[noOfVertices];
+        int[] postOrder = new int[noOfVertices];
+
+        int preCount = 0;
+        int postCount = 0;
+
+
+        // Helpers to compute DFS
+        int[] neighIndex = new int[noOfVertices];
+        int[] stack = new int[noOfVertices];
+        int stackSize = 0;
+
+        boolean[] visited = new boolean[noOfVertices];
+
+        for (int vId = 0; vId < noOfVertices; vId++)
+        {
+            parIds[vId] = -1;
+
+            preOrder[vId] = -1;
+            postOrder[vId] = -1;
+
+            visited[vId] = false;
+            neighIndex[vId] = 0;
+        }
+
+        // Push
+        stack[stackSize] = startId;
+        stackSize++;
+
+        while (stackSize > 0)
+        {
+            int vId = stack[stackSize - 1];
+            int nInd = neighIndex[vId];
+
+            if (nInd == 0)
+            {
+                visited[vId] = true;
+
+                // *** Pre-order for vId ***
+                preOrder[preCount] = vId;
+                preCount++;
+            }
+
+            if (nInd < g.edges[vId].length)
+            {
+                int neighId = g.edges[vId][nInd];
+
+                if (!visited[neighId])
+                {
+                    // Push
+                    stack[stackSize] = neighId;
+                    stackSize++;
+
+                    parIds[neighId] = vId;
+                }
+
+                neighIndex[vId]++;
+            }
+            else
+            {
+                // All neighbours checked, backtrack.
+                stackSize--; // Pop;
+
+                // *** Post-order for vId ***
+                postOrder[postCount] =  vId;
+                postCount++;
+            }
+        }
+
+        return new int[][]
+        {
+            parIds,
+            preOrder,
+            postOrder
+        };
+    }
+}
+
+
+
+
+
