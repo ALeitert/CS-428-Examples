ff2a11eda3f08eddc4cc0213abc9e022fab6a0e3
snofru <aleitert@kent.edu>
2021-04-30 14:36:56 +0200

    Added relax function.

--- ShortestPath.java
+++ ShortestPath.java
@@ -1,5 +1,41 @@
 
 public class ShortestPath
 {
+    /**
+     * Relexas an edge of the given graph based on the given distances.
+     * @param g A weighted graph.
+     * @param vId The vertex from which the edge starts.
+     * @param nIdx The index of the vertex in the neighbourhood of v to
+     *             which the edge points. That is, the edge goes from vId
+     *             to edges[vId][nIdx].
+     */
+    public static boolean relax(Graph g, int vId, int nIdx, int[] distances)
+    {
+        int uId = g.edges[vId][nIdx];
 
+        int uDis = distances[uId];
+        int vDis = distances[vId];
+
+        int vuWeight = g.weights[vId][nIdx];
+
+        boolean update = false;
+
+        // Avoid problems with overflow.
+        if (vuWeight > 0)
+        {
+            update = vDis < uDis - vuWeight;
+        }
+        else
+        {
+            update = vDis + vuWeight < uDis;
+        }
+
+        if (update)
+        {
+            distances[uId] = vDis + vuWeight;
+            return true;
+        }
+
+        return false;
+    }
 }
